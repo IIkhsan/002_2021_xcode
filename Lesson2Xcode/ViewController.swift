323658390781cf05bc626d5b98bc9efc2ab448a0
Ikhsanov Ilyas Raisovich <i.ikhsanov@tinkoff.ru>
2021-09-14 18:09:09 +0300

    Initial Commit

---
+++ Lesson2Xcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2Xcode
+//
+//  Created by i.ikhsanov on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
