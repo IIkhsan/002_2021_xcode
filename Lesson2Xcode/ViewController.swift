47f8d8b5f2745c6751392b2942ecad4f7fefe998
Daniil <daniilbagautdinov@yandex.ru>
2021-09-15 12:23:11 +0300

    added first screen; second and third view controllers

--- Lesson2Xcode/ViewController.swift
+++ Lesson2Xcode/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.backButtonTitle = "Назад"
     }
-
-
+    
+    
+    @IBAction func firstScreenButton(_ sender: Any) {
+        
+        
+    }
+    
+    
+    @IBAction func secondScreenButton(_ sender: Any) {
+    }
+    
 }
 
