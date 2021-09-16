da380081fc57fa4ff022d966ff787f64027cf15b
Daniil <daniilbagautdinov@yandex.ru>
2021-09-16 22:41:17 +0300

    added hw

--- Lesson2Xcode/SecondViewController.swift
+++ Lesson2Xcode/SecondViewController.swift
@@ -9,20 +9,28 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var switchq: UISwitch!
+    
+    let arr = ["ds","ab","qwe"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func button(_ sender: Any) {
+        swap()
     }
-    */
+    
+    
+    func swap() {
+        label.text = arr.randomElement()!
+        textField.text = arr.randomElement()!
+        switchq.isOn.toggle()
+    }
 
 }
--- Lesson2Xcode/ThirdViewController.swift
+++ Lesson2Xcode/ThirdViewController.swift
@@ -8,22 +8,61 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var firstPostImageView: UIImageView!
+    @IBOutlet weak var thirdStoryLabel: UILabel!
+    @IBOutlet weak var thirdStoryImageView: UIImageView!
+    
+    @IBOutlet weak var thirdPostImageView: UIImageView!
+    @IBOutlet weak var secondPostImageView: UIImageView!
+    @IBOutlet weak var secondStoryLabel: UILabel!
+    @IBOutlet weak var secondStoryImageView: UIImageView!
+    @IBOutlet weak var firstStoryLabel: UILabel!
+    @IBOutlet weak var firstStoryImageView: UIImageView!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    @IBOutlet weak var followersLabel: UILabel!
+    @IBOutlet weak var postsLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let photos = [UIImage(named: "1"),
+                  UIImage(named: "2"),
+                  UIImage(named: "3"),
+                  UIImage(named: "4"),
+                  UIImage(named: "5")]
+    
+    let nicknames = ["dead inside","gul","karbiz1234"]
+    
+    let titles = ["ya est gul", "hello world!","programmer"]
+    
+    let storyTitles = ["Leto","Zima","Vesna","Osen`"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        scrollView.contentSize = CGSize(width: 320, height: 1000)
+        mix()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func messageButton(_ sender: Any) {
+        mix()
     }
-    */
-
+    func mix() {
+        navigationItem.title = nicknames.randomElement()!
+        profileImageView.image = photos.randomElement()!
+        firstPostImageView.image = photos.randomElement()!
+        secondPostImageView.image = photos.randomElement()!
+        thirdPostImageView.image = photos.randomElement()!
+        postsLabel.text = "3"
+        followingLabel.text = "\(Int.random(in: 50...1000))"
+        followersLabel.text = "\(Int.random(in: 50...1000))"
+        bioLabel.text = titles.randomElement()!
+        firstStoryImageView.image = photos.randomElement()!
+        secondStoryImageView.image = photos.randomElement()!
+        thirdStoryImageView.image = photos.randomElement()!
+        firstStoryLabel.text = storyTitles.randomElement()!
+        secondStoryLabel.text = storyTitles.randomElement()!
+        thirdStoryLabel.text = storyTitles.randomElement()!
+    }
 }
