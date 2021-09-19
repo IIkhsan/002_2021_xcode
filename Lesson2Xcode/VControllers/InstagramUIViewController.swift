355db2c78568fa16abbb08d9f8862ab48707ff89
Ildarec <arslambekov63@gmail.com>
2021-09-19 19:02:48 +0300

    done

---
+++ Lesson2Xcode/VControllers/InstagramUIViewController.swift
@@ -0,0 +1,107 @@
+//
+//  InstagramUIViewController.swift
+//  InstagramUIViewController
+//
+//  Created by Ильдар Арсламбеков on 19.09.2021.
+//
+
+import UIKit
+
+class InstagramUIViewController: UIViewController {
+    
+    private var images: [UIImage] = [#imageLiteral(resourceName: "cezer"), #imageLiteral(resourceName: "pie"), #imageLiteral(resourceName: "sushi"), #imageLiteral(resourceName: "beer"), #imageLiteral(resourceName: "pizza"), #imageLiteral(resourceName: "kompot"), #imageLiteral(resourceName: "beshbarmak")]
+
+    @IBOutlet weak var accountNameLabel: UILabel!
+    @IBOutlet weak var addPostButton: UIButton!
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var postsCountLabel: UILabel!
+    @IBOutlet weak var followersCountLabel: UILabel!
+    @IBOutlet weak var subscribeCountLabel: UILabel!
+    @IBOutlet weak var realNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var subscribeButton: UIButton!
+    @IBOutlet weak var howToGetThereButton: UIButton!
+    @IBOutlet weak var writeMessageButton: UIButton!
+    @IBOutlet weak var callButton: UIButton!
+    
+    @IBOutlet weak var firstSavedImageView: UIImageView!
+    @IBOutlet weak var secondSavedImageView: UIImageView!
+    @IBOutlet weak var thirdSavedImageView: UIImageView!
+    
+    @IBOutlet weak var firstPublicationImageView: UIImageView!
+    @IBOutlet weak var secondPublicationImageView: UIImageView!
+    @IBOutlet weak var thirdPublicationImageView: UIImageView!
+    
+    @IBOutlet weak var fakeTapBarControllerView: UIView!
+    @IBOutlet weak var postsView: UIView!
+    
+    @IBAction func writeMessageButtonPressed(_ sender: Any) {
+        performChanges()
+    }
+    
+    override func viewDidLoad() {
+        editElements()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func performChanges() {
+        images.shuffle()
+        avatarImageView.image = images[0]
+        firstSavedImageView.image = images[1]
+        secondSavedImageView.image = images[2]
+        thirdSavedImageView.image = images[3]
+        firstPublicationImageView.image = images[4]
+        secondPublicationImageView.image = images[5]
+        thirdSavedImageView.image = images[6]
+        subscribeCountLabel.text = Int.random(in: 0..<100).description
+        followersCountLabel.text = Int.random(in: 0..<100).description
+        
+    }
+    
+    private func editElements() {
+        makeBordersGray(element: postsView)
+        makeBordersGray(element: fakeTapBarControllerView)
+        makeBordersGray(element: subscribeButton)
+        makeBordersGray(element: writeMessageButton)
+        makeBordersGray(element: howToGetThereButton)
+        makeBordersGray(element: callButton)
+        
+        makeImageViewRound(image: avatarImageView)
+        makeImageViewRound(image: firstSavedImageView)
+        makeImageViewRound(image: secondSavedImageView)
+        makeImageViewRound(image: thirdSavedImageView)
+        
+        makeBordersGray(element: avatarImageView)
+        makeBordersGray(element: firstSavedImageView)
+        makeBordersGray(element: secondSavedImageView)
+        makeBordersGray(element: thirdSavedImageView)
+        
+        subscribeButton.layer.cornerRadius = 5
+        writeMessageButton.layer.cornerRadius = 5
+        howToGetThereButton.layer.cornerRadius = 5
+        callButton.layer.cornerRadius = 5
+    }
+    
+    private func makeBordersGray(element: UIView) {
+        element.layer.borderWidth = 1
+        element.layer.borderColor = UIColor.systemGray4.cgColor
+    }
+    
+    private func makeImageViewRound(image: UIImageView) {
+        image.layer.cornerRadius = image.frame.size.width / 2
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lesson2Xcode/VControllers/UIElementsViewController.swift
@@ -0,0 +1,117 @@
+//
+//  UIElementsViewController.swift
+//  UIElementsViewController
+//
+//  Created by Ильдар Арсламбеков on 18.09.2021.
+//
+
+import UIKit
+import Foundation
+
+class UIElementsViewController: UIViewController, UITextFieldDelegate {
+    
+    private let pizzaImageName = "pizza"
+    private let beshbarmakImageName = "beshbarmak"
+    
+    @IBOutlet weak var changeSizeLabel: UILabel!
+    
+    @IBOutlet weak var onOffSwitcher: UISwitch!
+    @IBOutlet weak var onOffLabel: UILabel!
+    
+    @IBOutlet weak var changeBackgroundColorButton: UIButton!
+    
+    @IBOutlet weak var changeSwitchColorSwitch: UISwitch!
+    @IBOutlet weak var changeSwitchColorButton: UIButton!
+    
+    @IBOutlet weak var inputTextTextField: UITextField!
+    @IBOutlet weak var inputTextLabel: UILabel!
+
+    @IBOutlet weak var makeImageRoundedImageView: UIImageView!
+    @IBOutlet weak var makeImageRoundedSlider: UISlider!
+    
+    @IBOutlet weak var FirstImageView: UIImageView!
+    @IBOutlet weak var SecondImageView: UIImageView!
+    
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var datePickerTestLabel: UILabel!
+    @IBOutlet weak var datePickerTestDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var testProgressView: UIProgressView!
+    @IBOutlet weak var testSegmentControl: UISegmentedControl!
+    
+    @IBAction func inputTextTextFieldEndEditing(_ sender: UITextField) {
+        inputTextLabel.text = sender.text
+    }
+    
+    @IBAction func changeSwitchColorButtonPressed(_ sender: Any) {
+        changeSwitchColorSwitch.onTintColor = getRandomColor()
+    }
+    
+    @IBAction func changeSizeStepperChangedValueAction(_ sender: UIStepper) {
+        changeSizeLabel.font = UIFont.systemFont(ofSize: sender.value)
+    }
+    
+    @IBAction func onOffSwitchTriggered(_ sender: UISwitch) {
+                onOffLabel.isHidden = !sender.isOn
+    }
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        let color = getRandomColor()
+        view.backgroundColor = color
+        contentView.backgroundColor = color
+    }
+    
+    @IBAction func makeImageRoundedSliderValueChanged(_ sender: UISlider) {
+        makeImageRoundedImageView.layer.cornerRadius = CGFloat.init(sender.value * 100)
+    }
+    
+    @IBAction func alphaSliderChangeValue(_ sender: UISlider) {
+        FirstImageView.alpha = CGFloat.init(sender.value)
+        SecondImageView.alpha = 1 - CGFloat.init(sender.value)
+    }
+    @IBAction func datePickerChangedValue(_ sender: UIDatePicker) {
+        datePickerTestLabel.text = "Seconds: \(sender.countDownDuration.description)"
+    }
+    
+    @IBAction func stepperChangedValue(_ sender: UISegmentedControl) {
+        testProgressView.progress = Float((sender.selectedSegmentIndex + 1)) * 0.2
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.inputTextTextField.delegate = self
+        makeImageRoundedImageView.image = UIImage.init(named: beshbarmakImageName)
+        FirstImageView.image = UIImage.init(named: beshbarmakImageName)
+        FirstImageView.alpha = 0
+        SecondImageView.image = UIImage.init(named: pizzaImageName)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    private func getRandomColor() -> UIColor {
+         let red:CGFloat = CGFloat(drand48())
+         let green:CGFloat = CGFloat(drand48())
+         let blue:CGFloat = CGFloat(drand48())
+
+         return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
